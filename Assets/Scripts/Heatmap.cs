ba155fe31e2e062eac1f82a5452e8784d5262b64
Richard <richrdhw@gmail.com>
2020-06-24 17:39:47 -0400

    Initial Commit
    This is the foundation for the coop project

---
+++ Assets/Scripts/Heatmap.cs
@@ -0,0 +1,131 @@
+﻿// Alan Zucconi
+// www.alanzucconi.com
+using System.Collections;
+using System.Runtime.InteropServices;
+using UnityEngine; //Needed for struct layour
+public class Heatmap : MonoBehaviour
+{
+
+    [StructLayout(LayoutKind.Sequential),System.Serializable] //So bit stream is fixed for the GPU
+    [SerializeField]
+    private struct HeatData
+    {
+        public Vector3 position;
+        [Range(0f, 1f)]
+        public float radius;
+        [Range(0f, 1f)]
+        public float intensity;
+
+        public static int size = System.Runtime.InteropServices.Marshal.SizeOf(typeof(HeatData)); //Needed for compute buffer
+    }
+
+    [StructLayout(LayoutKind.Sequential), System.Serializable] //So bit stream is fixed for the GPU
+    [SerializeField]
+    private struct StoredHeatData
+    {
+        public Vector3 position;
+        [Range(0f, 1f)]
+        public float radius;
+        [Range(0f, 200f)]
+        public float intensity;
+        public static int size = System.Runtime.InteropServices.Marshal.SizeOf(typeof(StoredHeatData)); //Needed for compute buffer
+    }
+    
+    [SerializeField]
+    [Range(0f, 3f)]
+    private float radiusRatio = 1f; //Increase the radius of each point 
+    [SerializeField]
+    [Range(0f, 200f)]
+    private float intensityRatio = 1f; //Increase the stepping intensity
+    private ComputeBuffer buffer;
+
+    /*
+        This is pre allocated larger than we need so we dont need to reallocate on gpu/cpu, having to increase this array requires allocating on both PUs
+    */
+    [SerializeField]
+    private HeatData[] data = new HeatData[100];
+    [SerializeField]
+    private StoredHeatData[] storedData = new StoredHeatData[100];
+
+    //Material for Shader
+    public Material material;
+
+    //Size of the data array
+    public int count = 0;
+    public bool randomizePoints;
+    //Reading Data
+    void OnEnable() 
+    {
+        //Time.timeScale = .1f;
+        buffer = new ComputeBuffer(100, HeatData.size); //We can change count later but for now 100 per buffer
+        material.SetBuffer("_HeatData", buffer); //Sets it up on the shader
+
+        //Hardcoded random test set
+        for (int i = 0; i < data.Length; i++)
+        {
+            //Vector3 position = new Vector3(Random.Range(-1f, 1f),0, Random.Range(-.5f, .5f));
+            Vector3 position = new Vector3(Random.Range(-transform.localScale.x*.5f, transform.localScale.x * .5f),0f, Random.Range(-transform.localScale.y * .5f, transform.localScale.y * .5f));
+
+            data[i].position = position;
+            data[i].radius = Random.value *this.transform.localScale.z;//Small demo size
+            data[i].intensity = Random.value * this.transform.localScale.z;
+
+            storedData[i].position = data[i].position;
+            storedData[i].radius = data[i].radius;
+            storedData[i].intensity = data[i].intensity;
+        }
+        count = data.Length;
+        material.SetInt("_Count", count);
+
+        buffer.SetData(data); //SendsToShader, ***SETDATA() to change data
+    }
+    void Update()
+    {
+        Randomize();
+        //Update buffer data each frame
+        for (int i = 0; i < data.Length; i++)
+        {
+            //Vector3 position = new Vector3(data[i].position.x,transform.position.y, data[i].position.z);
+            //Vector3 position = new Vector3(Random.Range(-transform.localScale.x * .5f, transform.localScale.x * .5f), 0f, Random.Range(-transform.localScale.y * .5f, transform.localScale.y * .5f));
+            Vector3 position = new Vector3(storedData[i].position.x, transform.position.y, storedData[i].position.z);
+            data[i].position = position;
+            data[i].radius = storedData[i].radius * radiusRatio * 10f;//Small demo size
+            data[i].intensity = storedData[i].intensity * intensityRatio * 0.01f;
+
+        }
+        
+        buffer.SetData(data);
+
+    }
+
+    //For Debugging
+     void Randomize()
+     {
+         if(randomizePoints)
+         {
+            for (int i = 0; i < data.Length; i++)
+                {
+                //Vector3 position = new Vector3(Random.Range(-1f, 1f),0, Random.Range(-.5f, .5f));
+                Vector3 position = new Vector3(Random.Range(-transform.localScale.x*.5f, transform.localScale.x * .5f),0f, Random.Range(-transform.localScale.y * .5f, transform.localScale.y * .5f));
+
+                data[i].position = position;
+                data[i].radius = Random.value *this.transform.localScale.z;//Small demo size
+                data[i].intensity = Random.value * this.transform.localScale.z;
+                storedData[i].position = data[i].position;
+                storedData[i].radius = data[i].radius;
+                storedData[i].intensity = data[i].intensity;
+                }
+             randomizePoints = false;
+         }
+
+     }
+     void OnDisable() {
+
+        buffer.Dispose();
+
+     }
+    void OnApplicationQuit()
+    {
+        buffer.Dispose();
+    }
+}
---
+++ Assets/Scripts/sh_Heatmaps.shader
@@ -0,0 +1,95 @@
+﻿Shader "Maps/Heatmap" {
+	Properties{
+		_HeatTex("Gradient for Heatmap", 2D) = "white" {}
+		_BaseTex("Texture for Background", 2D) = "white" {}
+			 
+	}
+
+	SubShader{
+		Tags{ "Queue" = "Transparent" "DisableBatching"="True"}
+		Blend SrcAlpha OneMinusSrcAlpha
+
+		Pass {
+			ZWrite Off Cull Off
+			CGPROGRAM
+			#pragma vertex vert             
+			#pragma fragment frag
+			#include "UnityCG.cginc"
+
+			#define STEPS 64
+			#define STEP_SIZE 0.01
+			#define TEST_RADIUS 20
+
+			//Defining the heatpoints
+			struct heatData {
+				float3 pos : POSITION;
+				float rad : RADIUS;
+				float intensity : INTENSITY;
+			};
+
+			//Reading Screenspace
+			struct vertInput {
+				float4 pos : POSITION;
+			};
+
+			//Finialized Color
+			struct vertOutput {
+				float4 pos : POSITION;
+				fixed3 wPos : TEXCOORD0;
+				fixed3 worldPos : TEXCOORD1;
+			};
+
+			vertOutput vert(vertInput input) {
+				vertOutput o;
+				o.pos = UnityObjectToClipPos(input.pos);
+				o.worldPos = mul(unity_ObjectToWorld, input.pos).xyz;
+				return o;
+			}
+            
+			StructuredBuffer<heatData> _HeatData;
+			int _Count;
+
+			sampler1D _HeatTex;
+//
+			half4 frag(vertOutput output) : COLOR {
+				//fill color to surface
+				
+				// Loops over all the points
+				half h = 0;
+
+				for (int i = 0; i < _Count; i++)
+				{
+					// Calculates the contribution of each point
+					half di = distance(output.worldPos, _HeatData[i].pos);
+					half ri = _HeatData[i].rad;
+					half hi = 1 - saturate(di / ri);
+
+					h += hi * _HeatData[i].intensity;
+				}
+
+				// Converts (0-1) according to the heat texture
+				h = saturate(h);
+				half4 color = tex1D(_HeatTex, h);
+				return color;
+			}
+			ENDCG
+		}
+
+	}
+	Fallback "Diffuse"
+}
+			// bool SphereHit(float3 p, float3 center, float1 radius)
+			// {
+			// 	return distance(p, center) < radius;
+			// };
+
+			// float RaymarchHit(float3 position, float3 direction)
+			// {
+			// 	for(int i = 0; i< STEPS; i++)
+			// 	{
+			// 		if(SphereHit(position, float3(0,0,0), TEST_RADIUS*2))
+			// 			return 1;
+			// 		position += direction * STEP_SIZE;
+			// 	}
+			// 	return 0;
+			// }
