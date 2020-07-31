eb851486a840d07c650ecc61106c2a581ee105e8
Carsten Booth <bootcars@sheridancollege.ca>
2020-07-31 12:50:25 -0400

    Added District Details Script

---
+++ Assets/Scripts/UI/DistrictDetails.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistrictDetails : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void UpdateDistrictDetails()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
