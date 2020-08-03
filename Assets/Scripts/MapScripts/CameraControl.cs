ba088565bba0309d9d679488e5ac28d98c3b48db
Richard <richrdhw@gmail.com>
2020-08-03 13:35:40 -0400

    UI info update

--- Assets/Scripts/MapScripts/CameraControl.cs
+++ Assets/Scripts/MapScripts/CameraControl.cs
@@ -2,10 +2,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+
 //[ExecuteInEditMode]
 public class CameraControl : MonoBehaviour
 {
-        public enum ControlMode
+    public enum ControlMode
     {
         FullMap,
         Zoomed,
@@ -14,14 +16,14 @@ public class CameraControl : MonoBehaviour
     [SerializeField]
     private GameObject activeCamera = null;
     [Header("Camera Settings")]
-    [Range (0.1f, 5f), SerializeField]
+    [Range(0.1f, 5f), SerializeField]
     private float rotateSpeed = 1f;
-    [Range (1f, 5f), SerializeField]
+    [Range(1f, 5f), SerializeField]
     private float panSpeed = 1f;
     [SerializeField]
-    private AnimationCurve MotionCurve = null; 
+    private AnimationCurve MotionCurve = null;
     [SerializeField]
-    private Vector3 cameraMaxZoom = new Vector3(0, 120, -120); 
+    private Vector3 cameraMaxZoom = new Vector3(0, 120, -120);
 
     [Header("References")]
     [SerializeField]
@@ -38,30 +40,45 @@ public class CameraControl : MonoBehaviour
     [SerializeField]
     private ControlMode currentMode = ControlMode.FullMap;
     [SerializeField]
-    private GameObject targetLocation = null;
+    public GameObject targetLocation = null;
     [SerializeField]
     private Vector3 targetPosition;
     public bool resetCam;
-    [Range (0f, 1f), SerializeField]
+    [Range(0f, 1f), SerializeField]
     public float zoomLerp = 1f;
 
     private float cameraLerpTimer = 0f;
 
     [SerializeField]
     private Quaternion targetRotation = Quaternion.identity;
-    private Quaternion eighty = Quaternion.Euler(80,0,0);
-    private Quaternion fourfive = Quaternion.Euler(45,0,0);
+    private Quaternion eighty = Quaternion.Euler(80, 0, 0);
+    private Quaternion fourfive = Quaternion.Euler(45, 0, 0);
     private Quaternion zero = Quaternion.identity;
     void Update()
     {
-    MoveCamera();
+        MoveCamera();
+        ScrollwheelZoom();
     }
 
+    void ScrollwheelZoom()
+    {
+        if (EventSystem.current.IsPointerOverGameObject())//If cursor is on UI, return
+            return;
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f) // forward
+        {
+            zoomLerp += 0.175f;
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f) // backwards
+        {
+            zoomLerp -= 0.175f;
+        }
+
+    }
     void MoveCamera()
     {
-        if((int)currentMode == 1)
+        if ((int)currentMode == 1)
         {
-            if(resetCam)
+            if (resetCam)
             {
                 //targetLocation = cameraFullMap;
                 //targetRotation = eighty;
@@ -69,40 +86,34 @@ public class CameraControl : MonoBehaviour
                 cameraLerpTimer = 0f;
                 resetCam = false;
             }
-            if (Input.GetAxis("Mouse ScrollWheel") > 0f ) // forward
+
+            if (Input.GetMouseButton(1))
             {
-                zoomLerp +=0.175f;
-            }             
-            if (Input.GetAxis("Mouse ScrollWheel") < 0f ) // backwards
-            {
-                zoomLerp -=0.175f;
-            }
-            if(Input.GetMouseButton(1))
-            {
-                if(Input.GetAxis("Mouse X")>0 ||Input.GetAxis("Mouse X")<0)
+                if (Input.GetAxis("Mouse X") > 0 || Input.GetAxis("Mouse X") < 0)
                 {
-                cameraZoomed.transform.Rotate(0, (Input.GetAxis("Mouse X")) * rotateSpeed, 0, Space.Self);
+                    cameraZoomed.transform.Rotate(0, (Input.GetAxis("Mouse X")) * rotateSpeed, 0, Space.Self);
                 }
             }
-            if(Input.GetMouseButton(2))
+            if (Input.GetMouseButton(2))
             {
-                Vector3 mouseInputOffset = new Vector3(Input.GetAxis("Mouse X"),0,Input.GetAxis("Mouse Y"));
-                targetPosition -= cameraZoomed.transform.TransformDirection(mouseInputOffset)*panSpeed;
+                Vector3 mouseInputOffset = new Vector3(Input.GetAxis("Mouse X"), 0, Input.GetAxis("Mouse Y"));
+                targetPosition -= cameraZoomed.transform.TransformDirection(mouseInputOffset) * panSpeed;
             }
         }
 
-        if((int)currentMode == 2)
+        if ((int)currentMode == 2)
         {
 
         }
 
         zoomLerp = Mathf.Clamp(zoomLerp, 0, 1);
-        if(cameraLerpTimer<1f){
-        cameraLerpTimer += Time.deltaTime*0.5f;
+        if (cameraLerpTimer < 1f)
+        {
+            cameraLerpTimer += Time.deltaTime * 0.5f;
         }
         zoomedCameraRig.transform.localPosition = Vector3.Slerp(cameraMaxZoom, Vector3.zero, MotionCurve.Evaluate(zoomLerp));
-        TheCamera.transform.localRotation = Quaternion.Lerp(TheCamera.transform.localRotation,targetRotation, MotionCurve.Evaluate(cameraLerpTimer));
-        activeCamera.transform.position = Vector3.Lerp(activeCamera.transform.position,targetPosition,MotionCurve.Evaluate(cameraLerpTimer));
+        TheCamera.transform.localRotation = Quaternion.Lerp(TheCamera.transform.localRotation, targetRotation, MotionCurve.Evaluate(cameraLerpTimer));
+        activeCamera.transform.position = Vector3.Lerp(activeCamera.transform.position, targetPosition, MotionCurve.Evaluate(cameraLerpTimer));
     }
 
     public void SwitchToFullMap()
@@ -125,32 +136,32 @@ public class CameraControl : MonoBehaviour
     }
 
 
-    
+
 }
-    //[SerializeField]
-    //public event Action onSwitchCamera;
-    // ControlMode CheckCurrentCamera ()
-    // {
-    //     activeCamera.SetActive(false); 
+//[SerializeField]
+//public event Action onSwitchCamera;
+// ControlMode CheckCurrentCamera ()
+// {
+//     activeCamera.SetActive(false); 
 
-    //         if((int)currentMode == 0)
-    //         {
-    //             activeCamera = cameraFullMap;
-    //         }
-    //         if((int)currentMode == 1)
-    //         {
-    //             if(VFXHandler.LastClickedObject == null)
-    //             {
-    //             currentMode = ControlMode.FullMap;
-    //             }else {
-    //             activeCamera = cameraZoomed;
-    //             }
-    //         }
-    //         if((int)currentMode == 2)
-    //         {
-    //             activeCamera = cameraFreelook;
-    //         }
-    //     activeCamera.SetActive(true); 
+//         if((int)currentMode == 0)
+//         {
+//             activeCamera = cameraFullMap;
+//         }
+//         if((int)currentMode == 1)
+//         {
+//             if(VFXHandler.LastClickedObject == null)
+//             {
+//             currentMode = ControlMode.FullMap;
+//             }else {
+//             activeCamera = cameraZoomed;
+//             }
+//         }
+//         if((int)currentMode == 2)
+//         {
+//             activeCamera = cameraFreelook;
+//         }
+//     activeCamera.SetActive(true); 
 
-    //     return currentMode;
-    // }
+//     return currentMode;
+// }
--- Assets/Scripts/UI/NeighborhoodButton.cs
+++ Assets/Scripts/UI/NeighborhoodButton.cs
@@ -6,18 +6,33 @@ public class NeighborhoodButton : MonoBehaviour
 {
     //[HideInInspector]
     public DistrictDetails m_detailPanel;
+    public DistrictReference m_reference;
     public CameraControl m_cameraControlScript;
+    public BorderVFXHandler m_borderVFXHandler;
 
     private void Start()
     {
         m_detailPanel = FindObjectOfType<DistrictDetails>();
+        m_reference = FindObjectOfType<DistrictReference>();
         m_cameraControlScript = FindObjectOfType<CameraControl>();
+        m_borderVFXHandler = FindObjectOfType<BorderVFXHandler>();
+
     }
     public void Clicked()       // copy this for selecting district in map
     {
         m_detailPanel.m_districtToBeCalled = name.ToString();
         m_detailPanel.UpdateDistrictDetails();
 
-        m_cameraControlScript.SwitchToZoom();
+        //GameObject m_targetDistrict = null;
+        for (int i = 0; i < m_reference.Districts.Count; i++)
+        {
+            if (m_reference.Districts[i].name == m_detailPanel.m_districtToBeCalled)
+            {
+                m_borderVFXHandler.LastClickedObject = m_reference.Districts[i];
+                m_borderVFXHandler.SwitchDistrict(i);
+                m_cameraControlScript.SwitchToZoom();// Swtich camera mode to zoomed
+                break;
+            }
+        }
     }
 }
