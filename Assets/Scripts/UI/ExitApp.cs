6258e9993072a7e44a39a0f4bf6d1a369c4107e3
Carsten Booth <bootcars@sheridancollege.ca>
2020-08-04 15:28:11 -0400

    Added Exit Button and News Link button

---
+++ Assets/Scripts/UI/ExitApp.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitApp : MonoBehaviour
+{
+    public void ExitApplication()
+    {
+        Application.Quit();
+    }
+
+
+}
---
+++ Assets/Scripts/UI/OpenExternalLink.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenExternalLink : MonoBehaviour
+{
+    public string url;
+
+    public string selectedDistrict;
+    string findDistrict;
+
+    public DistrictDetails districtDetails;
+    public cs_CSVData m_csvData;
+
+
+    private void Start()
+    {
+        districtDetails = FindObjectOfType<DistrictDetails>();
+    }
+
+    public void OpenURL()
+    {
+
+        selectedDistrict = districtDetails.m_title.text.ToString();
+
+        url = "http://www.google.com/search?q=" + "Toronto " + selectedDistrict + " " + "COVID-19";
+
+        Application.OpenURL(url);
+    }
+}
